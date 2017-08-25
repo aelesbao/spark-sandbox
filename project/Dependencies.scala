5435a0683d473660e393ae984328010a1843b319
Augusto Elesbão <aelesbao@gmail.com>
2017-08-25 12:47:37 +0200

    Add Scala Logging

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,5 +1,11 @@
 import sbt._
 
 object Dependencies {
+  lazy val logback = "ch.qos.logback" % "logback-classic" % "1.2.3"
+  lazy val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.7.2"
+
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.1"
+
+  lazy val logging = Seq(logback, scalaLogging)
+  lazy val testing = Seq(scalaTest % Test)
 }
