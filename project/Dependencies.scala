5148f6cd4071bb7f7b2c0b5d1c4a599d60d5f825
Augusto Elesbão <aelesbao@gmail.com>
2017-10-10 00:17:55 +0200

    Configure sbt-assembly plugin to build fat JAR

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,13 +2,14 @@ import sbt._
 
 object Dependencies {
   lazy val sparkVersion = "2.2.0"
-  lazy val sparkCore = "org.apache.spark" %% "spark-core" % sparkVersion exclude("org.slf4j", "slf4j-log4j12")
+  lazy val sparkCore = "org.apache.spark" %% "spark-core" % sparkVersion % "provided" exclude("org.slf4j", "slf4j-log4j12")
 
   lazy val logback = "ch.qos.logback" % "logback-classic" % "1.2.3"
   lazy val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.7.2"
 
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.4"
 
+  lazy val sparkDeps = Seq(sparkCore)
   lazy val loggingDeps = Seq(logback, scalaLogging)
   lazy val testingDeps = Seq(scalaTest % Test)
 }
